8bb1523d2d8f178e217ab002ede9602fa177a091
Andreas Gronbeck <mac-battleship@Andreass-MacBook-Air.local>
2025-08-21 19:22:06 +0200

    Added budget and date verification

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import matplotlib.pyplot as plt
 import json
 import os
+from datetime import datetime
 
-transaction_id = 1
+
 
 try:
     with open("transactions.json", "r") as f:
-        transactions = json.load(f) if os.path.getsize("transactions.json") > 0 else []
+        data = json.load(f) if os.path.getsize("transactions.json") > 0 else {"budget": 0, "transactions": []}
+        transactions = data["transactions"]
         transaction_id = transactions[-1]["id"] if transactions else 0
+        budget = data["budget"] if "budget" in data else 0
 except FileNotFoundError:
     transactions = []
     transaction_id = 0
+    budget = 0
 
 def main():
-    global transactions, transaction_id
+    global transactions, transaction_id, budget
     while True:
-        print("Please choose your option (type the number):\n 1. Add transcation: \n 2. Remove transaction:  \n 3. List transactions: \n 4. Plotting \n 5. Exit \n 6. Delete all data")
+        print("Please choose your option (type the number):\n 1. Add transcation: \n 2. Remove transaction:  \n 3. List transactions: \n 4. Plotting \n 5. Exit \n 6. Delete all data \n 7. Set new budget" )
         option = input()
         if option == "1":
             print("\n")
-            add_transaction(float(input("How much?: ")), input("What category?: "), input("Which date?: "))
+            add_transaction(float(input("How much?: ")), input("What category?: "), input("Which date? (press enter for today): "))
             print("\n")
 
         elif option == "2":
@@ -34,7 +38,7 @@ def main():
         
         elif option == "5":
             with open("transactions.json", "w") as f:
-                json.dump(transactions, f, indent=4) 
+                json.dump({"budget": budget, "transactions": transactions}, f, indent=4) 
             break
 
         elif option == "6":
@@ -44,19 +48,36 @@ def main():
                 os.remove("transactions.json")
             except Exception:
                 pass
+        
+        elif option == "7":
+            budget = float(input("Set a new budget amount: "))
 
 
-def add_transaction(amount, category, date):
-    global transaction_id
-    transaction_id += 1
-    transactions.append(
-        {
-        "id": transaction_id,
-        "amount": amount, 
-        "category": category, 
-        "date":date
-        }
-        )
+def add_transaction(amount, category, date=None):
+    if date == None or date == "":
+        date = datetime.now()
+    if isinstance(date, datetime):
+        date = date.strftime("%d-%m-%Y")
+    real_date = verify_date(date)
+    if real_date:
+        global transaction_id
+        transaction_id += 1
+        transactions.append(
+            {
+            "id": transaction_id,
+            "amount": amount, 
+            "category": category, 
+            "date":date
+            }
+            )
+    
+def verify_date(date):
+    try:
+        datetime.strptime(date, "%d-%m-%Y")
+        return True
+    except ValueError:
+        print("Invalid date, use the d-m-y format")
+        return False
     
 
 def remove_transaction(index):
@@ -67,12 +88,16 @@ def remove_transaction(index):
         print("\nInvalid index\n")
 
 def list_transactions():
+    total = 0
     if not transactions:
         print("\nEmpty!\n")
         return
     print("\n")
     for transaction in transactions:
         print(f"{transaction["id"]}. [{transaction['date']}, ${transaction['amount']}, - {transaction['category']}]")
+        total += transaction["amount"]
+    print(f"\n You have a budget of {budget}, and have spent {total}. Your remaining total is {budget-total} \n")
+        
 
 
 def plotting():
